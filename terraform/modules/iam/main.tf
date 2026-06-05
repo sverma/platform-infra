f540a0dc62e46361eae410c9261e7e613603aae3
saurabh verma <saurabhverma@Gauravs-MacBook-Air.local>
2026-06-05 15:35:59 +0530

    Adding certificate

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -25,3 +25,27 @@ resource "google_service_account_iam_member" "workload_identity" {
   member = "serviceAccount:${var.project_id}.svc.id.goog[external-dns/external-dns]"
 }
 
+resource "google_service_account" "cert_manager" {
+
+  account_id = "cert-manager"
+}
+
+
+resource "google_project_iam_member" "cert_manager_dns" {
+
+  project = var.project_id
+
+  role = "roles/dns.admin"
+
+  member = "serviceAccount:${google_service_account.cert_manager.email}"
+}
+
+
+resource "google_service_account_iam_member" "cert_manager_workload_identity" {
+
+  service_account_id = google_service_account.cert_manager.name
+
+  role = "roles/iam.workloadIdentityUser"
+
+  member = "serviceAccount:${var.project_id}.svc.id.goog[cert-manager/cert-manager]"
+}
