cf6a8f6f39c5c7b2f02f8fee2e858d243c216112
saurabh verma <saurabhverma@Gauravs-MacBook-Air.local>
2026-06-07 22:04:42 +0530

    Fixing the argocd error and adding correct paths

--- terraform/modules/argocd-bootstrap/main.tf
+++ terraform/modules/argocd-bootstrap/main.tf
@@ -21,11 +21,38 @@ resource "helm_release" "argocd" {
   ]
 }
 
-resource "kubectl_manifest" "root_app" {
+resource "kubectl_manifest" "root_app_stage" {
 
   depends_on = [
     helm_release.argocd
   ]
 
-  yaml_body = file("${path.module}/root-app.yaml")
+  yaml_body = file("${path.module}/root-app-stage.yaml")
+}
+
+resource "kubectl_manifest" "root_app_prod" {
+
+  depends_on = [
+    helm_release.argocd
+  ]
+
+  yaml_body = file("${path.module}/root-app-prod.yaml")
+}
+
+resource "kubectl_manifest" "root_app_platform_stage" {
+
+  depends_on = [
+    helm_release.argocd
+  ]
+
+  yaml_body = file("${path.module}/root-app-plaform-stage.yaml")
+}
+
+resource "kubectl_manifest" "root_app_platform_prod" {
+
+  depends_on = [
+    helm_release.argocd
+  ]
+
+  yaml_body = file("${path.module}/root-app-platform-prod.yaml")
 }
